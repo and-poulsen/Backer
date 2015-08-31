da4de4019aab3c9ceb7cfc51fbd4a4564524c2e4
lanatra <Anden702@gmail.com>
2015-08-31 20:55:58 +0200

    Can browse rooms/trees and download files

--- routes/index.js
+++ routes/index.js
@@ -39,10 +39,10 @@ router.get('/', function(req, res, next) {
 				if(c == 0)
 					data.rooms.push({
 						name: $(this).find('tr td a').text(),
-						link: $(this).find('tr td a').attr('href')
+						link: "/room/" + $(this).find('tr td a').attr('href').substring($(this).find('tr td a').attr('href').indexOf("edit=") + 6)
 					})
 			})
-
+			console.log(data.rooms);
 			res.render('index', data);
 		})
 });
@@ -60,78 +60,169 @@ router.get('/room/:id', function(req, res, next) {
 				https.get({
 					host: 'fronter.com',
 					path: '/cphbusiness/prjframe/index.phtml?orderid=0&toolprjid=' + req.params.id + '&logstat_toolid=25&mo=2',
-					headers: {
-						'Cookie': req.headers.cookie,
-						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-						'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-						// 'Accept-Encoding': 'gzip, deflate, sdch',
-						'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
-						'Host': 'fronter.com',
-						'Cache-Control': 'max-age=0',
-						'Upgrade-Insecure-Requests': 1
-					}
+					headers: createHeaders(req)
 				}, function(response) {
-					console.log(response.headers);
-					console.log(response.statusCode);
 					response.on('data', function(d) {
 						html += d.toString();
 					})
 				}).on('error', function(e) {
 					console.log("Got error: " + e.message);
 				}).on('close', function() {
-					console.log(html);
-		// $ = cheerio.load(html);
+					$ = cheerio.load(html);
 
-		// var data = {
-		// 	name: $('a.ctrl-button').first().text(),
-		// 	rooms: []
-		// }
-
-		// 	// Rooms
-		// 	$('.fav-room-list-element.content-frame-content').each(function(i,e) {
-		// 		var c = 0;
-		// 		for (var i = 0; i < data.rooms.length; i++) {
-		// 			if(data.rooms[i].name == $(this).find('tr td a').text())
-		// 				c++;
-		// 		};
-		// 		if(c == 0)
-		// 			data.rooms.push({
-		// 				name: $(this).find('tr td a').text(),
-		// 				link: $(this).find('tr td a').attr('href')
-		// 			})
-		// 	})
-				res.send(html);
-			// res.render('index', data);
-		})
+					var data = {
+						rooms: []
+					}
+					// Rooms
+					$('.mybookmarks-element tr td.tablelist-text a').each(function(i,e) {
+						var c = 0;
+						for (var i = 0; i < data.rooms.length; i++) {
+							if(data.rooms[i].name == $(this).text())
+								c++;
+						};
+						if(c == 0) {
+							var l = $(this).attr('href');
+							l = l.substring(l.indexOf('iid=') + 4);
+							l = '/tree/' + l.substring(0, l.indexOf('&sechash'));
+							data.rooms.push({
+								name: $(this).text(),
+								link: l
+							})
+						}
+					})
+					// res.send(html);
+					res.render('index', data);
+				})
 			})
-})
-})
-
-
-	console.log(req.headers.cookie)
-	// https.get({
-	// 	host: 'fronter.com',
-	// 	path: '/cphbusiness/navbar.phtml?goto_prjid=' + req.params.id,
-	// 			headers: {
-	// 		'Cookie': req.headers.cookie,
-	// 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-	// 		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-	// 		'Accept-Encoding': 'gzip, deflate, sdch',
-	// 		'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
-	// 		Host: 'fronter.com',
-	// 		'Cache-Control': 'max-age=0',
-	// 		'Upgrade-Insecure-Requests': 1
-	// 	}
-	// }, function(response) {
-	// 	console.log(response.headers);
-	// 	console.log(response.statusCode);
-	// 	response.on('data', function(d) {
-	// 	})
-	// }).on('close', function() {
-
-
-	// })
+		})
+	})
 });
+
+// Tree
+router.get('/tree/:id', function(req, res, next) {
+	var html;
+
+	https.get({
+		host: 'fronter.com',
+		path: '/cphbusiness/links/structureprops.phtml?temp=temp&init_load=1&treeid=' + req.params.id,
+		headers: createHeaders(req)
+	}, function(response) {
+		response.on('data', function(d) {
+			html += d.toString();
+		})
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	}).on('close', function() {
+		$ = cheerio.load(html);
+
+		var data = {
+			rooms: []
+		}
+
+		// Folders
+		// console.log($('.content-of-selected-folder').text());
+		$('.content-of-selected-folder tr td label a').each(function(i,e) {
+			var c = 0;
+			for (var i = 0; i < data.rooms.length; i++) {
+				if(data.rooms[i].name == $(this).text())
+					c++;
+			};
+			if(c == 0) {
+				var link = $(this).attr('href'), type;
+
+				if(link.contains('treeid')) {
+					type = 'tree';
+					link = '/tree/' + getId(link, 'treeid=', '');
+				} else if(link.contains('url_redirect')) {
+					type = 'redirect';
+					link = '/redirect/' + getId(link, 'id=', '')
+				} else if(link.contains('/files.phtml')) {
+					type = 'file';
+					link = '/file?path=' + encodeURIComponent(getId(link, 'files.phtml/', ''))
+				} else if(link.contains('player/index.php')) {
+					type = 'file';
+					link = '/file?path=' + getId(link,'files.phtml%2F', '');
+				}
+
+				data.rooms.push({
+					name: $(this).text(),
+					link: link,
+					type: type
+				})
+			}
+		})
+		res.render('index', data);
+	})
+
+});
+
+
+// File
+router.get('/file', function(req, res, next) {
+	var html;
+	// if(req.query.path.contains())
+	https.get({
+		host: 'fronter.com',
+		path: '/cphbusiness/links/files.phtml/' + req.query.path,
+		headers: createHeaders(req)
+	}, function(response) {
+		res.writeHeader(response.statusCode, response.headers);
+		response.pipe(res);
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	}).on('close', function() {
+		// res.send("hi");
+		// res.render('index', data);
+	})
+
+});
+
+// Redirect
+router.get('/redirect/:id', function(req, res, next) {
+	var html;
+	https.get({
+		host: 'fronter.com',
+		path: '/cphbusiness/links/url_redirect.phtml?id=' + req.params.id,
+		headers: createHeaders(req)
+	}, function(response) {
+		response.on('data', function(d) {
+			html += d.toString();
+		})
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	}).on('close', function() {
+		console.log(html);
+		console.log("REDIRECT TO: " + getId(html,' location.replace("', '");'));
+		res.redirect(getId(html,'location.replace("', '");'));
+		// res.render('index', data);
+	})
+
+});
+
+
+
+
+
+
+
+
+var getId = function(string, before, after) {
+	string.replace(/\n/g, '');
+	string = string.substring(string.indexOf(before) + before.length);
+	if(after.length > 0)
+		return string.substring(0, string.indexOf(after));
+	return string;
+		
+}
+
+
+
+
+if (!('contains' in String.prototype)) {
+  String.prototype.contains = function(str, startIndex) {
+    return ''.indexOf.call(this, str, startIndex) !== -1;
+  };
+}
 
 
 var quickGet = function(url, req, cb) {
@@ -139,16 +230,7 @@ var quickGet = function(url, req, cb) {
 	https.get({
 		host: 'fronter.com',
 		path: url,
-		headers: {
-			'Cookie': req.headers.cookie,
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-			'Accept-Encoding': 'gzip, deflate, sdch',
-			'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
-			'Host': 'fronter.com',
-			'Cache-Control': 'max-age=0',
-			'Upgrade-Insecure-Requests': 1
-		}
+		headers: createHeaders(req)
 	}, function(response) {
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
@@ -159,5 +241,18 @@ var quickGet = function(url, req, cb) {
 }
 
 
+var createHeaders = function(req) {
+	var r = {
+		'Cookie': req.headers.cookie,
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		// 'Accept-Encoding': 'gzip, deflate, sdch',
+		'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
+		'Host': 'fronter.com',
+		'Cache-Control': 'max-age=0',
+		'Upgrade-Insecure-Requests': 1
+	};
+	return r;
+}
 
 module.exports = router;
